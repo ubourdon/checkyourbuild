97363b4375697885c4c1244605d03b748cb0782d
ubourdon <bourdon.ugo@gmail.com>
2012-09-13 16:16:11 +0200

    add: naïve implementation of Server-Sent Event (SSE)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,40 @@
 package controllers
 
 import play.api._
+import libs.iteratee._
+import play.api.libs.json._
+import play.api.libs.json.Json._
 import play.api.mvc._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok( views.html.index() )
-  }
-  
+
+    var state = true
+
+    def index = Action {
+        Ok( views.html.index() )
+    }
+
+    def check = Action {
+        var event = ""
+
+        if( state ) {
+            state = false
+            event = "event: init\ndata: \n\n"
+        } else {
+            state = true
+            event = "event: ok\ndata: \n\n"
+        }
+
+        println( "test event : " + event )
+
+        Ok( event ).as( "text/event-stream" )
+    }
+
+    def initBuild( idBuild: String ) = Action {
+        Ok( idBuild )
+    }
+    
+    def endBuild( urlXmlResult: String ) = Action {
+        Ok( urlXmlResult )
+    }
 }
